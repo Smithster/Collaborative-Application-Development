2779596d7f3adee4f962db0457271e16844bdf73
smithster <samsmith99@btinternet.com>
2023-04-21 21:40:41 +0100

    removed unused code

--- static/graphFunctions.js
+++ static/graphFunctions.js
@@ -11,7 +11,6 @@ const removeWarning = () => {
 const checkError = () => {
   FromDate = document.getElementById('FromDate').value
   StartDate = document.getElementById('StartDate').value
-  // StartBookings = document.getElementById('StartBookings').value
 
   if (!(FromDate < StartDate)){
     return true
@@ -43,7 +42,6 @@ const test = () => {
   EventName = document.getElementById('EventName').value
   GroupSize = document.getElementById('GroupSize').value
   StartDate = document.getElementById('StartDate').value
-  // StartBookings = document.getElementById('StartBookings').value
   FromDate = document.getElementById('FromDate').value
   eventType = document.getElementById('EventType').value
   
@@ -53,7 +51,6 @@ const test = () => {
     'EventName' : EventName,
     'GroupSize' : GroupSize,
     'StartDate' : StartDate,
-    // 'StartBookings' : StartBookings,
     'FromDate' : FromDate,
     'EventType' : eventType
   }
